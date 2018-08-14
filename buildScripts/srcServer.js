62de059c2093b91ee0969bb67c60ef6f07731584
Ryan Cutter <ryancutter.mail@gmail.com>
2018-08-14 07:28:05 +0100

    basic api setup with fetch

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -21,6 +21,15 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../src/index.html"));
 });
 
+app.get("/users", function(req, res) {
+  //hard coded DB representation
+  res.json([
+    { id: "1", firstName: "Bobby", lastName: "Smith", email: "test1@test.com" },
+    { id: "2", firstName: "Tammy", lastName: "Jones", email: "test2@test.com" },
+    { id: "3", firstName: "Tina", lastName: "Brown", email: "test3@test.com" }
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
